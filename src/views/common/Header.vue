da9cb4f0ff3985c47eb9c96cd04178e3ded79121
MuSheng <425893642@qq.com>
2017-08-14 17:30:25 +0800

    产品页模版完成

--- src/views/common/Header.vue
+++ src/views/common/Header.vue
@@ -2,10 +2,7 @@
   <header class="header">
     <div class="header-content">
       <h1 class="logo">
-        <a href="/" class="logo-img">
-          <!--<img src="../../assets/images/logo.png" alt="dji">-->
-          <!--<span class="title">{{ $t('projectTitle') }}</span>-->
-        </a>
+        <a href="/" class="logo-img"></a>
       </h1>
 
       <a href="javascript:;" class="menu" @click="onToggleMenuClick" >
@@ -14,21 +11,6 @@
 
       <top-nav></top-nav>
 
-      <!--<el-dropdown @command="handleCommand" class="operate" trigger="click">-->
-        <!--<a href="javascript:;" class="account">-->
-          <!--<icon name="account"></icon>-->
-          <!--<span>{{ user.username }}</span>-->
-          <!--<span class="caret"></span>-->
-        <!--</a>-->
-        <!--<el-dropdown-menu slot="dropdown" class="dropmenu">-->
-          <!--<el-dropdown-item  command="Switch">-->
-            <!--<icon class="icons" name="switch"></icon>{{ $t('switchLang') }}-->
-          <!--</el-dropdown-item>-->
-          <!--<el-dropdown-item divided command="Logout">-->
-            <!--<icon class="icons" name="logout"></icon>{{ $t('signOut') }}-->
-          <!--</el-dropdown-item>-->
-        <!--</el-dropdown-menu>-->
-      <!--</el-dropdown>-->
     </div>
   </header>
 </template>
@@ -86,176 +68,134 @@ export default {
 </script>
 
 <style type="text/css" lang="scss">
-@import './../../assets/scss/variables.scss';
-@import './../../assets/scss/mixins.scss';
+  @import './../../assets/scss/variables.scss';
 
-#app .header {
-  /*position: fixed;*/
-  position: relative;
-  z-index: 9;
-  height: $header-height;
-  width: 100%;
-  min-width: 320px;
-  background-color: $header-color;
-  @include clearfix();
-
-  .header-content {
-    width: $topnav-width;
-    margin: 0 auto;
-    text-align: center;
-    display: flex;
-    flex-flow: row nowrap;
-    justify-content: center;
-  }
-
-  a {
-    &:hover, &:focus {
-      text-decoration: none;
-      span {
-        text-decoration: none;
-      }
-    }
-  }
-  .logo {
-    float: left;
-    padding: 6px 30px 14px 0;
-
-    img {
-      display: inline-block;
-      vertical-align: middle;
-      width: 36px;
-      height: auto;
-      margin-right: 10px;
-    }
-
-    .logo-img {
-      background: url("../../assets/images/logo.png");
-      display: inline-block;
-      height: 21px;
-      width: 140px;
-      transition: all 0.75s ease;
-    }
-    .logo-img:hover{
-      opacity: 0.8;
-    }
-  }
-  .title {
-    display: inline-block;
-    vertical-align: middle;
-    font-size: $font-large;
-    color: #fff;
-  }
-  .menu {
-    display: none;
-    position: absolute;
-    top: 0;
-    left: 0;
-    padding: 20px 15px 10px;
-    width: 60px;
-    height: 60px;
-  }
-  .menu:hover {
-    background: $header-color;
-  }
-  .notification {
-    float: right;
-    padding: 20px;
-
-    &:hover {
-      background-color: $header-hover-color;
-    }
-  }
-  .operate {
-    float: right;
-    position: relative;
-    &:hover {
-      .dropmenu {
-        display: block;
-        width: 100%;
-        min-width: 150px !important;
-      }
-      > a {
-        background-color: $header-hover-color;
-      }
-    }
-    > a {
-      display: inline-block;
-      padding: 20px;
-      color: #fff;
-    }
-    .active {
-      background-color: $header-hover-color;
-    }
-    .avatar {
-      display: inline-block;
-      vertical-align: middle;
-      width: 32px;
-      height: 32px;
-      border-radius: 50%;
-    }
-  }
-}
-
-.menu span:after, .menu span:before {
-  content: "";
-  position: absolute;
-  left: 0;
-  top: 9px;
-}
-.menu span:after {
-  top: 18px;
-}
-.menu span {
-  position: relative;
-  display: block;
-}
-.menu span, .menu span:after, .menu span:before {
-  width: 100%;
-  height: 3px;
-  background-color: #fff;
-  -webkit-transition: all 0.3s;
-  backface-visibility: hidden;
-}
-.menu-expand .menu span {
-  background-color: transparent;
-}
-.menu-expand .menu span:before {
-  transform: rotate(45deg) translate(2px, 2px);
-}
-.menu-expand .menu span:after {
-  transform: rotate(-45deg) translate(4px, -5px);
-}
-
-@media (max-width: #{$responsive-width-small}) {
   #app .header {
-    .menu {
-      display: block;
-    }
+    /*position: fixed;*/
+    position: relative;
+    z-index: 9;
+    height: $header-height;
+    width: 100%;
+    min-width: 320px;
+    background-color: $header-color;
 
-    .logo {
-      text-align: center;
-      @include absolute-center;
-      float: none;
-      img {
-        margin-right: 5px;
-      }
-    }
-    .operate {
-      .account {
-        max-width: 80px;
-        text-overflow: ellipsis;
-        overflow: hidden;
-        white-space: nowrap;
-      }
-    }
     .header-content {
       width: 100%;
       height: 100%;
+      margin: 0 auto;
+      text-align: center;
+      display: flex;
+      flex-flow: row nowrap;
+      justify-content: center;
+    }
+
+    a {
+      &:hover, &:focus {
+        text-decoration: none;
+        span {
+          text-decoration: none;
+        }
+      }
+    }
+
+    .menu {
+      display: block;
+      position: absolute;
+      top: 0;
+      left: 0;
+      padding: 20px 15px 10px;
+      width: 60px;
+      height: 60px;
+    }
+    .menu:hover {
+      background: $header-color;
+    }
+
+    .logo {
+      float: left;
+      padding: 6px 30px 14px;
+
+      img {
+        display: inline-block;
+        vertical-align: middle;
+        width: 36px;
+        height: auto;
+        margin-right: 10px;
+      }
+
+      .logo-img {
+        background: url("../../assets/images/logo.png");
+        display: inline-block;
+        height: 21px;
+        width: 140px;
+        transition: all 0.75s ease;
+      }
+
+      .logo-img:hover{
+        opacity: 0.8;
+      }
+    }
+
+    .active {
+      background-color: $header-hover-color;
     }
   }
 
   .menu-expand .menu {
     background-color: $header-color;
   }
-}
+
+
+  .menu span:after, .menu span:before {
+    content: "";
+    position: absolute;
+    left: 0;
+    top: 9px;
+  }
+  .menu span:after {
+    top: 18px;
+  }
+  .menu span {
+    position: relative;
+    display: block;
+  }
+  .menu span, .menu span:after, .menu span:before {
+    width: 100%;
+    height: 3px;
+    background-color: #fff;
+    -webkit-transition: all 0.3s;
+    backface-visibility: hidden;
+  }
+  .menu-expand .menu span {
+    background-color: transparent;
+  }
+  .menu-expand .menu span:before {
+    transform: rotate(45deg) translate(2px, 2px);
+  }
+  .menu-expand .menu span:after {
+    transform: rotate(-45deg) translate(4px, -5px);
+  }
+
+  @media (min-width: #{$responsive-width-small}) {
+    #app .header {
+
+      .header-content {
+        width: $topnav-width;
+
+        .logo {
+          padding: 6px 30px 14px 0;
+          text-align: center;
+          float: none;
+          img {
+            margin-right: 5px;
+          }
+        }
+      }
+
+      .menu {
+        display: none;
+
+      }
+    }
+  }
 </style>
