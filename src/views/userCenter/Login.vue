2650e991627d380738d0cb3ea81c62d40be44261
MuSheng <425893642@qq.com>
2017-09-22 11:35:54 +0800

    个人中心登录页面

--- src/views/userCenter/Login.vue
+++ src/views/userCenter/Login.vue
@@ -12,13 +12,17 @@
             <el-input v-model="form.password" placeholder="请输入密码" type="password">
             </el-input>
           </el-form-item>
-          <el-button type="primary" @click="onSubmit" :loading="buttonSubmit.loading">登 录</el-button>
+          <div class="function-button">
+            <a class="" href="javascript:void(0);" @click="resetPassword">忘记密码 >></a>
+            <el-button type="primary" class="submit-btn" @click="onSubmit" :loading="buttonSubmit.loading">登 录</el-button>
+          </div>
         </el-form>
       </div>
       <hr>
       <div class="login-info">
         <h5>尊敬的E人E本用户：</h5>
         <p>欢迎您使用E本社区，登录社区前请先激活E人E本“个人中心”。成功激活后，使用激活手机号，即可在本页面登录。</p>
+        <a class="" href="javascript:void(0);" @click="validateInfo">验证设备信息 >></a>
       </div>
     </section>
   </div>
@@ -71,7 +75,20 @@
 
       onSubmit() {
         console.log('submit!');
-      }
+        this.buttonSubmit.loading = true;
+        const othis = this;
+        setTimeout(function() {
+          othis.buttonSubmit.loading = false;
+        }, 2000);
+      },
+
+      resetPassword() {
+        console.log('resetPassword');
+      },
+
+      validateInfo() {
+        console.log('validateInfo');
+      },
     },
 
   }
@@ -87,20 +104,42 @@
       margin: 0 auto;
     }
 
+    a {
+      color: $char-color;
+      line-height: 36px;
+    }
+
+    a:hover {
+      color: $char-hover-color;
+    }
+
     section.page {
 
       .login-form {
-        height: 240px;
+        height: 300px;
         max-width: 400px;
         text-align: center;
         margin: 0 auto;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
 
         h4 {
           padding: 20px 0;
+          text-align: left;
         }
 
         .form-content {
 
+          .function-button {
+            display: flex;
+            justify-content: space-between;
+
+            .submit-btn {
+              height: 36px;
+              width: 90px;
+            }
+          }
         }
       }
 
@@ -110,7 +149,7 @@
       }
 
       .login-info {
-        height: 240px;
+        height: 200px;
         display: flex;
         flex-direction: column;
         justify-content: center;
@@ -126,6 +165,9 @@
           line-height: $common-line-height;
         }
 
+        a {
+          width: 110px;
+        }
       }
     }
   }
@@ -141,6 +183,7 @@
         display: flex;
 
         .login-form {
+          height: 240px;
           width: 50%;
           padding: 0 20px 0 0;
         }
@@ -151,9 +194,9 @@
         }
 
         .login-info {
+          height: 240px;
           width: 50%;
           padding: 0 0 0 20px;
-          justify-content: flex-start;
         }
       }
     }
