c4f2ba5fc60a7ade79b6d189a30e4b82c840796c
MuSheng <425893642@qq.com>
2017-10-18 15:49:19 +0800

    修复iphone上safari无痕模式浏览页面时，无法使用localStorage导致的问题，判断无法使用时改为用cookie存储

--- src/helper/utils.js
+++ src/helper/utils.js
@@ -53,7 +53,67 @@ export default {
     return url + '?' + str.join('&')
   },
 
-  /* -----------------------------localStorage------------------------------------ */
+  /**
+   * 判断浏览器是否支持本地存储
+   * @returns {boolean}
+   */
+  isLocalStorageSupported() {
+    let testKey = 'test',
+      storage = window.sessionStorage;
+    try {
+      storage.setItem(testKey, 'testValue');
+      storage.removeItem(testKey);
+      return true;
+    } catch (error) {
+      return false;
+    }
+  },
+
+  /* ----------------------------- cookie ----------------------------- */
+  /**
+   * 作者：陈楠酒肆
+   * 链接：http://www.jianshu.com/p/ccbadea30e30
+   * 來源：简书
+   * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+   */
+  /**
+   * 存储cookie
+   * @param name cookie名称
+   * @param value cookie值
+   * @param days 存放天数
+   */
+  setCookie (name, value, days = 1000) {
+    let eDate = new Date();
+    eDate.setDate(eDate.getDate() + days);
+    document.cookie = name + "=" + escape(value) + ((days == null) ? "" : ";expires=" + eDate.toGMTString());
+  },
+
+  /**
+   * 获取cookie
+   * @param name
+   * @returns {*}
+   */
+  getCookie(name) {
+    let arr, reg = new RegExp("(^| )" + name + "=([^;]*)(;|$)");
+    if (arr = document.cookie.match(reg))
+      return (arr[2]);
+    else
+      return null;
+  },
+
+  /**
+   * 删除cookie
+   * @param name
+   */
+  delCookie (name) {
+    let exp = new Date();
+    exp.setTime(exp.getTime() - 1);
+    let cval = getCookie(name);
+    if (cval != null)
+      document.cookie = name + "=" + cval + ";expires=" + exp.toGMTString();
+  },
+
+  /* ----------------------------- localStorage ------------------------------------ */
   /**
    * 存放一条本地存储记录
    * @param name
@@ -64,7 +124,13 @@ export default {
     if (typeof content !== 'string') {
       content = JSON.stringify(content)
     }
-    window.localStorage.setItem(name, content)
+    if (this.isLocalStorageSupported()) {
+      // supported localStorage
+      window.localStorage.setItem(name, content)
+    } else {
+      // unsupported, use cookie
+      this.setCookie(name, content);
+    }
   },
 
   /**
@@ -73,7 +139,12 @@ export default {
    */
   getStorage (name) {
     if (!name) return;
-    let getContent = window.localStorage.getItem(name);
+    let getContent = false;
+    if (this.isLocalStorageSupported()) {
+      getContent = window.localStorage.getItem(name);
+    } else {
+      getContent = this.getCookie(name);
+    }
     if(!getContent) return;
     try {
       // content有可能不是JSON字符串而是纯字符串
@@ -89,7 +160,11 @@ export default {
    */
   removeStorage (name) {
     if (!name) return;
-    window.localStorage.removeItem(name)
+    if (this.isLocalStorageSupported()) {
+      window.localStorage.removeItem(name)
+    } else {
+      this.delCookie(name);
+    }
   },
 
   /**
